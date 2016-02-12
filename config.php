0608d069103ae24ce0721967f43f6b03df66951c
Claire Warnes <zed.excalibur@gmail.com>
2016-02-12 13:52:41 +0000

    Change config

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 ini_set( "display_errors", true );
-date_default_timezone_set( "Europe/London" );  
+date_default_timezone_set( "Europe/London" );
 define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_NAME',getenv('OPENSHIFT_APP_NAME'));
 define( "CLASS_PATH", "classes" );
 define( "TEMPLATE_PATH", "templates" );
 define( "HOMEPAGE_NUM_ARTICLES", 5 );
-define( "ADMIN_USERNAME", "admin" );
-define( "ADMIN_PASSWORD", '$2y$10$PmsUPHXQ4xsrr74LrjdhuOCkZQxbXa3.c.WzmvSMMN7Z8Rr8QRS/u' );
+define( "ADMIN_USERNAME", "myusername" );
+define( "ADMIN_PASSWORD", 'hashofmypassword' );
 require( CLASS_PATH . "/Article.php" );
 function handleException( $exception ) {
   echo "Sorry, a problem occurred. Please try later.";
